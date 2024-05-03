3d054327e4627fbbacdb94a1b0cf5624d454947a
miiieagle <miieagle06@gmail.com>
2024-05-03 18:56:13 +0300

    Express router

--- 3_Express_Router/index.js
+++ 3_Express_Router/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const setupRoutes = require("./statup/routes");
+const setupRoutes = require("./statup/routes");  //routing
 const port = process.env.PORT;
 //adding application midware
 app.use(express.urlencoded({extended: true}))
 app.use(express.json({}));
 
-setupRoutes(app);
+setupRoutes(app); //calling of router function
 //port listening
 app.listen(port, () => {
     console.log("listening on port:" + port)
