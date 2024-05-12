49756c9138763852617c06fa5a1fc75fd6640bcc
miiieagle <miieagle06@gmail.com>
2024-05-12 21:27:37 +0300

    validation

---
+++ 6_validation/routes/users.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const userModel = require("../models/users")  //importing the users from the models
+const router = express.Router();
+
+
+//Validation for email and password
+router.post("/register", async(req, res, next) => {
+    const {email, password} = req.body
+    if(!email){
+        return res.status(400).json({
+            msg: "email is required",
+        })
+    }
+    
+    if(!password){
+        return res.status(400).json({
+            msg: "password is required",
+        })
+    }
+    else{
+
+    }
+    res.status(201).json({ 
+        "message": "Account created successfully",
+        
+    });
+});
+
+
+//getting users from data base
+router.get("/", async(req, res, next) => {
+    const userExist = await userModel.find({})
+    res.status(200).json({
+        data:userExist
+    })
+})
+
+//getting one parameter from data base
+router.get("/email", async(req, res, next) => {
+    const {email} = req.body
+    const userExist = await userModel.findOne({email:email})
+    res.status(200).json({
+        data:userExist
+    })
+})
+
+module.exports = router;
